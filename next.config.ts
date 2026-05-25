448498d62d51d38395fc0bf998938154bb8d23d6
jakehu <srbmladen@proton.me>
2026-05-25 18:51:00 +0800

    fix: disable image optimization globally to support redirected local images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["pg"],
   allowedDevOrigins: ["localhost", "127.0.0.1", "10.131.152.213"],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
